b013c11c767a48b364305dd39949da73aa2e160a
Cam <>
2026-08-20 09:04:28 -0600

    Practicas js

---
+++ Praticas js/script.js
@@ -0,0 +1,24 @@
+document.write("Hola mundo");
+ 
+alert("Mundo Hola");
+ 
+console.log("Hola chavales");
+ 
+//Tipos de datos
+let nombre = "Ana";
+ 
+let edad = 18;
+ 
+let estudiante = true;
+ 
+let ciudad;
+ 
+let mascota = null;
+ 
+//Usando typeof para identificar cada tipo
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof estudiante);
+console.log(typeof ciudad);
+console.log(typeof mascota);
+ 
